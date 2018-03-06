e96b27d5a6cd23973328c586fc9fd414e83276f3
MikeModder <15313620+MikeModder@users.noreply.github.com>
2018-03-06 10:50:34 -0500

    Add JSDoc, update gitignore

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+/**
+ Teapot Tools Module.
+ * @module teapot-toolsjs
+ */
+
 'use strict';
 const {Builder, By, until} = require('selenium-webdriver');
 
-let TeapotTools = class TeapotTools {
+/** TeapotTools Module */
+class TeapotTools {
+    /** 
+     * Create a new instance of TeapotTools.
+    */
     constructor(){
         this.driver = new Builder().forBrowser('firefox').build();
         this.driver.get('https://google.com/teapot');
@@ -10,14 +19,27 @@ let TeapotTools = class TeapotTools {
         this.teapot = this.driver.findElement({ id: 'teabot' });
     }
 
+    /** 
+    * Rotate the teapot.
+    * @param {Number} degrees How many degrees to rotate the teapot.
+    * @returns {Promise} any
+    */
     rotate(degrees){
         return this.driver.executeScript(`arguments[0].setAttribute('style', 'transform: rotate(${degrees}deg);')`, this.teapot);
     }
 
+    /**
+    * Pour the tea from the teapot.
+    * @returns {Promise} any
+    */
     pour(){
         return this.driver.executeScript(`arguments[0].setAttribute('class', 'tip pour')`, this.teapot);
     }
     
+    /**
+    * Stop pouring the tea.
+    * @returns {Promise} any
+    */
     unpour(){
         return this.driver.executeScript(`arguments[0].setAttribute('class', '')`, this.teapot);
     }
